9f374bcfc3c9120f193c0219496da90e9a7f5ff3
fgrazzini70 <fgrazzini@arpae.it>
2021-04-13 18:09:34 +0200

    Rivisto calcolo picchi con calcolo valori medi su picchi multipli per ciascun caso

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib.dates as md
 import pandas as pd
 import numpy as np
 from scipy.stats import pearsonr
+from scipy.signal import argrelextrema
 
 
 def assign_prec(w1, shift):
@@ -32,17 +33,21 @@ def search_runoff_start(w1, p15):
             count = 0
     return start
 
+def nearest_date(items,pivot):
+    nearest=min(items, key=lambda x: abs(x - pivot))
+    return nearest
 
 if __name__ == '__main__':
     # read test case
-    fileName = r'.\INPUT\Test_nov2017.csv'
+    fileName = r'.\INPUT\Test_feb2015.csv'
     df = pd.read_csv(fileName)
 
     # parameters
     WHC = df.WHC[0]     # [mm] water holding capacity
-    delta = 1         # [hours] shift estimation - observed
+    delta = 0         # [hours] shift estimation - observed
     alpha = 0.1         # [-]  time factor
     k_soil = 1.6        # [mm/hour] infiltration
+    ts_liv = 0.2  # soglia idrometrica sotto la quale non non considero i massimi
 
     # index = date
     df['date'] = pd.to_datetime(df['Dataf'])
@@ -76,11 +81,6 @@ if __name__ == '__main__':
     # forecast water level
     df['estLevel'] = 3.8 / (1 + 20 * np.exp(-0.15 * runoff)) - 0.15
 
-    # compute the anticipation time of the peak
-    time_max_o = df.Livello.idxmax()
-    time_max_f = df.estLevel.idxmax()
-    delta_max = (time_max_f - time_max_o) / np.timedelta64(1, 'h')  # in hours
-
     # clean dataset (only event)
     df_event = df[df.index >= r_start]
     df_clean = df_event[df_event['Livello'].notna()]
@@ -92,6 +92,36 @@ if __name__ == '__main__':
     df_shift = df_clean.shift(-delta, freq='h')
     df_obs = df_shift[['Livello']]
 
+    # searching for multiple peaks and relative statistics
+
+    idmaxrel_o = argrelextrema(df_clean.Livello.values, np.greater)  # indice dei massimi relativi osservati
+    maxO = df_clean.Livello[df_clean.index[idmaxrel_o]]  # valori dei massimi relativi osservati
+    idmaxrel_f = argrelextrema(df_clean.estLevel.values, np.greater)  # indice del massimi relativi previsti
+    maxF = df_clean.estLevel[df_clean.index[idmaxrel_f]]  # valori dei massimi relativi previsti
+
+    # df dei valori massimi
+    frame = {'maxO': maxO, 'maxF': maxF}
+    df_max = pd.DataFrame(frame)
+    df_max = df_max[df_max >= ts_liv].dropna(axis=0, how='all')
+    array_do = df_max.maxO.dropna()
+    array_df = df_max.maxF.dropna()
+
+    dt = []
+    dm = []
+    listaoss = []
+    listafo = []
+    # loop sulle date dei massimi osservati per associarli con i previsti
+    for i in array_do.index:
+        nearest = nearest_date(array_df.index, i)  # trovo l'ora del picco più vicino a quello osservato
+        listaoss.append(i)
+        listafo.append(nearest)
+        dt.append((nearest - i) / np.timedelta64(1, 'h'))  # calcolo il time shift
+        dm.append(df_max.maxF[nearest] - df_max.maxO[i])  # calcolo l'errore
+
+    # mean peaks characteristics
+    mPeak_anti = np.mean(dt)
+    mPeak_err = np.mean(dm)
+
     df_visu = df_est.join(df_obs, how='outer').dropna()
 
     xo = df_visu.index
@@ -100,11 +130,12 @@ if __name__ == '__main__':
 
     r, p_value = pearsonr(yo, ye)
     RMSE = np.sqrt(((ye - yo) ** 2).mean())
-    peakError = ye.max() - yo.max()
+
 
     #print
-    print("WHC: ", WHC, "  K: ", k_soil)
-    print("Runoff start: ", r_start, "  Observed peak: ", time_max_o, " Forecast peak: ", time_max_f, )
+    print("WHC: ", WHC, "  K: ", k_soil, "  Runoff start: ", r_start)
+    print("Observed peaks: ", listaoss)
+    print("Forecast peaks: ", listafo)
 
     # plot
     plt.figure(figsize=(10, 5))
@@ -119,7 +150,7 @@ if __name__ == '__main__':
     ax.plot(xo, ye, label='Estimated')
     ax.set_ylabel('water level [m]')
     plt.title('R=' + str(round(r, 3)) + '   RMSE[m]=' + str(round(RMSE, 2))
-              + '   Peak error[m]=' + str(round(peakError, 2)) + '  Peak anticipation[h]= '
-                                       + str(round(delta_max, 2)), size=12)
+              + '   mPeak error[m]=' + str(round(mPeak_err, 2)) + '  mPeak anticipation[h]= '
+                                       + str(round(mPeak_anti, 2)), size=12)
     plt.legend()
     plt.show()
